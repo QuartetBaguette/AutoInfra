6dc452d852eb102fa50531a116af1b71838bebfe
Hedys <77706906+QuartetBaguette@users.noreply.github.com>
2025-02-05 20:51:03 +0100

    Split up file, finished Week1

---
+++ Week1/instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-0c614dee691cbbf37"
+  instance_type          = "t2.micro"
+  key_name               = "ruben"
+  vpc_security_group_ids = ["${aws_security_group.ssh_http_https_allow.id}"]
+  subnet_id              = aws_subnet.PublicSubnet1.id
+
+  tags = {
+    Name = "Web1"
+  }
+}
--- Week1/network.tf
+++ Week1/network.tf
@@ -76,31 +76,3 @@ resource "aws_vpc_security_group_ingress_rule" "Allow_SSH" {
   ip_protocol       = "tcp"
   to_port           = 22
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  key_name               = "ruben"
-  vpc_security_group_ids = ["${aws_security_group.ssh_http_https_allow.id}"]
-  subnet_id              = aws_subnet.PublicSubnet1.id
-
-  tags = {
-    Name = "Web1"
-  }
-}
